689e8e1d14a6688153a45ab7ea3e383b296afe2a
plexoio <>
2023-05-16 13:31:20 +0000

    add validation check for 6 input digits

--- run.py
+++ run.py
@@ -6,12 +6,26 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+
+def validate_data(values):
+    '''
+    Validate user's first input to get sales data
+    '''
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'6 values required, you input {len(values)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again\n')
+
 
 def get_data_sales():
     '''
@@ -22,6 +36,8 @@ def get_data_sales():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input('Enter your data here: ')
-    print(f'The data provided is {data_str}')
+    sales_data = data_str.split(',')  # the array is here
+    validate_data(sales_data)  # array to validate
+
 
 get_data_sales()
